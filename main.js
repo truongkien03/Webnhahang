7d9dc53722f1e50762900edc72468e2e242d10eb
truongkien03 <104815240+truongkien03@users.noreply.github.com>
2022-06-25 16:31:51 +0700

    .

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
+// load
 window.addEventListener("load", () => {
     const loader = document.querySelector(".loader");
-
     loader.classList.add("loader--hidden");
-
     loader.addEventListener("transitionend", () => {
         document.body.removeChild(loader);
     });
